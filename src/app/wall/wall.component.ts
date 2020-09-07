a27acb86e4a76040a875dcfa4e8a31f9929fd1a2
MOHITdasila <dasila.m24@gmail.com>
2020-09-07 11:26:53 +0530

    start my wall

---
+++ src/app/wall/wall.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-wall',
+  templateUrl: './wall.component.html',
+  styleUrls: ['./wall.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class WallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
